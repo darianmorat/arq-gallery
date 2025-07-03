e8139d963d2d216d5d22b4b8be927078bfae0edc
darianmorat <yojhandariantoledomora@gmail.com>
2025-07-03 08:53:23 -0500

    fix(client): base project setup for connection to db

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,30 @@
+import { GalleryVerticalEnd } from "lucide-react";
 import { LoginForm } from "@/components/login-form";
 
 export const Login = () => {
    return (
-      <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-         <div className="w-full max-w-sm md:max-w-3xl">
-            <LoginForm />
+      <div className="grid min-h-svh lg:grid-cols-2">
+         <div className="flex flex-col gap-4 p-6 md:p-10">
+            <div className="flex justify-center gap-2 md:justify-start">
+               <a href="/" className="flex items-center gap-2 font-medium">
+                  <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                     <GalleryVerticalEnd className="size-4" />
+                  </div>
+                  Arq Gallery
+               </a>
+            </div>
+            <div className="flex flex-1 items-center justify-center">
+               <div className="w-full max-w-xs">
+                  <LoginForm />
+               </div>
+            </div>
+         </div>
+         <div className="bg-muted relative hidden lg:block">
+            <img
+               src="https://images.unsplash.com/photo-1502082553048-f009c37129b9"
+               alt="Image"
+               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.25] dark:grayscale"
+            />
          </div>
       </div>
    );
