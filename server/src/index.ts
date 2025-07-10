5835896ae1fc504a9003684fe06828d993103ccd
darianmorat <yojhandariantoledomora@gmail.com>
2025-07-10 08:54:30 -0500

    fix(index): add cookie and cors setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route";
+import adminRoute from "./routes/admin.route";
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+   cors({
+      credentials: true,
+      origin: process.env.CLIENT_URL,
+   }),
+);
 
 app.use("/auth", authRoute);
+app.use("/admin", adminRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
