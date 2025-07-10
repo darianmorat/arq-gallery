2d88a92eb9e6786fb65b20c582ee2cbfec837226
darianmorat <yojhandariantoledomora@gmail.com>
2025-07-10 09:04:23 -0500

    fix(seeds): add testing data for users

--- server/src/db/seeds/users.ts
+++ server/src/db/seeds/users.ts
@@ -1,23 +1,26 @@
 import { db } from "../index";
 import { userTable } from "../schema";
 
+// REMEMBER: USING THIS SEED JUST FOR INFORMATION LIKE PRODUCTS, ETC NEVER FOR
+// USERS AR ANYTHING THAT CAN HURT THE SECURITY OF THE APLICATION
+
 export const seedUsers = async () => {
    const userData = [
       {
          name: "John Doe",
          email: "john@example.com",
-         password: "hashedPassword123",
+         password: "$2b$10$2PmDkuLmez5JEK9rZmUAdujF6MW3n6/xbJijKW4l4LwquwxQJrvlG", // admin123
          role: "admin",
       },
       {
          name: "Jane Smith",
          email: "jane@example.com",
-         password: "hashedPassword456",
+         password: "$2b$10$OBXey0PsP5rSNxZlaQAmuOwKKoYckp9RAQ5L3fBb9zAOd235t4a3W", // 1082124119
       },
       {
          name: "Bob Johnson",
          email: "bob@example.com",
-         password: "hashedPassword789",
+         password: "$2b$10$r6UmwTOBUmwxpJo8u5cWeuoNYtiR2o5d4GfKKjNkjtPSyUoorIqUu", // 1082124119
       },
    ];
 
