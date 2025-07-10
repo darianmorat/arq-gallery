e1fc862b366d01424a1fcca30302063c278dc9ff
darianmorat <yojhandariantoledomora@gmail.com>
2025-07-10 10:06:19 -0500

    fix(pages): update createdAt variable with useFormatDate hook

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { useFormatDate } from "@/hooks/useFormatDate";
 import { useAuthStore } from "@/stores/useAuthStore";
 
 export const Dashboard = () => {
    const { user, logout } = useAuthStore();
+   const createdAt = useFormatDate(user?.createdAt);
 
    if (!user) {
       return <div>Cargando...</div>;
@@ -11,11 +13,10 @@ export const Dashboard = () => {
    return (
       <>
          <h1 className="text-xl">Admin page</h1>
-         <div>id: {user.id}</div>
          <div>name: {user.name}</div>
          <div>email: {user.email}</div>
          <div>role: {user.role}</div>
-         <div>created at: {user.createdAt}</div>
+         <div>created at: {createdAt}</div>
          <Button variant={"destructive"} onClick={() => logout()}>
             Logout
          </Button>
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,16 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { useFormatDate } from "@/hooks/useFormatDate";
 import { useAuthStore } from "@/stores/useAuthStore";
 
 export const Profile = () => {
    const { user, logout } = useAuthStore();
-
-   const createdAtDate = (date: string) => {
-      return new Date(date).toLocaleDateString("en-US", {
-         year: "numeric",
-         month: "long",
-         day: "numeric",
-      });
-   };
+   const createdAt = useFormatDate(user?.createdAt);
 
    if (!user) {
       return <div>Cargando...</div>;
@@ -20,7 +14,7 @@ export const Profile = () => {
       <>
          <div>name: {user.name}</div>
          <div>email: {user.email}</div>
-         <div>created at: {createdAtDate(user.createdAt)}</div>
+         <div>created at: {createdAt}</div>
          <Button variant={"destructive"} onClick={() => logout()}>
             Logout
          </Button>
