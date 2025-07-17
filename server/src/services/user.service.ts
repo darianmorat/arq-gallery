6277a5ef84617fe477f56f11f744a2d83b836f07
darianmorat <yojhandariantoledomora@gmail.com>
2025-07-17 09:36:33 -0500

    fix(service): update user operations

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { eq } from "drizzle-orm";
 import { db } from "../db";
 import { userTable } from "../db/schema";
+import { genSalt, hash } from "bcrypt-ts";
 
 export const userService = {
    findByEmail: async (email: string) => {
@@ -27,6 +28,48 @@ export const userService = {
          .from(userTable)
          .where(eq(userTable.id, id))
          .limit(1);
+
+      return user;
+   },
+
+   // Admin Operations
+   getAll: async () => {
+      const users = await db
+         .select({
+            id: userTable.id,
+            name: userTable.name,
+            email: userTable.email,
+         })
+         .from(userTable)
+         .where(eq(userTable.role, "user"));
+      return users;
+   },
+
+   create: async (name: string, email: string, password: string) => {
+      const salt = await genSalt(10);
+      const hashedPassword = await hash(password, salt);
+
+      const [user] = await db
+         .insert(userTable)
+         .values({
+            name,
+            email,
+            password: hashedPassword,
+         })
+         .returning({
+            id: userTable.id,
+            name: userTable.name,
+            email: userTable.email,
+         });
+
+      return user;
+   },
+
+   delete: async (id: string) => {
+      const [user] = await db
+         .delete(userTable)
+         .where(eq(userTable.id, id))
+         .returning({ name: userTable.name });
 
       return user;
    },
